822cf078cca1c0374871c8c9152eaefe83021586
junoade <ajchoi0928@gmail.com>
2025-11-25 23:32:04 +0900

    fix: @RequestBody 누락 확인 후 수정

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/order/OrderV1Controller.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/order/OrderV1Controller.java
@@ -6,6 +6,7 @@ import com.loopers.application.order.UserOrderProductFacade;
 import com.loopers.interfaces.api.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class OrderV1Controller implements OrderV1ApiSpec {
 
     @PostMapping("/preOrder")
     @Override
-    public ApiResponse<OrderV1Dto.OrderResponse> preOrder(OrderV1Dto.OrderRequest request) {
+    public ApiResponse<OrderV1Dto.OrderResponse> preOrder(@RequestBody OrderV1Dto.OrderRequest request) {
         OrderCommand.Order orderCmd = request.toCommand();
         OrderResult.PreOrderResult preOrderResult = userOrderProductFacade.preOrder(orderCmd);
         return ApiResponse.success(OrderV1Dto.OrderResponse.fromPreOrder(preOrderResult));
@@ -25,7 +26,7 @@ public class OrderV1Controller implements OrderV1ApiSpec {
 
     @PostMapping("/placeOrder")
     @Override
-    public ApiResponse<OrderV1Dto.OrderResponse> placeOrder(OrderV1Dto.OrderRequest request) {
+    public ApiResponse<OrderV1Dto.OrderResponse> placeOrder(@RequestBody OrderV1Dto.OrderRequest request) {
         OrderCommand.Order orderCmd = request.toCommand();
         OrderResult.PlaceOrderResult placeOrderResult = userOrderProductFacade.placeOrder(orderCmd);
         return ApiResponse.success(OrderV1Dto.OrderResponse.fromOrderPlacement(
