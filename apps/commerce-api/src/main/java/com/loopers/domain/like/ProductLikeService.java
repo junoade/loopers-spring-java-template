0fe6d51c85f5050a60d2b39caede02c2a4914c09
junoade <ajchoi0928@gmail.com>
2025-11-26 22:06:15 +0900

    feat: 상품 목록 조회 서비스 개선

--- apps/commerce-api/src/main/java/com/loopers/domain/like/ProductLikeService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/like/ProductLikeService.java
@@ -1,12 +1,6 @@
 package com.loopers.domain.like;
 
-import com.loopers.domain.product.ProductSortType;
-import com.loopers.infrastructure.productLike.ProductLikeQueryRepository;
-import com.loopers.support.error.CoreException;
-import com.loopers.support.error.ErrorType;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,7 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 public class ProductLikeService {
     private final ProductLikeRepository productLikeRepository;
-    private final ProductLikeQueryRepository productLikeQueryRepository;
 
     @Transactional
     public int like(Long userPkId, Long productId) {
@@ -24,17 +17,6 @@ public class ProductLikeService {
     @Transactional
     public void dislike(Long userPkId, Long productId) {
         productLikeRepository.delete(userPkId, productId);
-    }
-
-    @Transactional(readOnly = true)
-    public ProductLikeSummaryVO getProductLikeSummary(Long productId) {
-        return productLikeQueryRepository.findProductDetail(productId)
-                .orElseThrow(() -> new CoreException(ErrorType.NOT_FOUND, "존재하지 않는 상품입니다"));
-    }
-
-    @Transactional(readOnly = true)
-    public Page<ProductLikeSummaryVO> productLikeSummaryVOPage(ProductSortType sortType, Pageable pageable) {
-        return productLikeQueryRepository.findProductLikes(sortType, pageable);
     }
 
 }
