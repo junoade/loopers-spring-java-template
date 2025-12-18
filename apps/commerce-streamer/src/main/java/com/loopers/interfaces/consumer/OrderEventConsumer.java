d9038b72f54c1f7299767e1375551f29dd83b6a2
junoade <ajchoi0928@gmail.com>
2025-12-19 01:34:22 +0900

    feat: OrderEventConsumer 간단하게 구현

---
+++ apps/commerce-streamer/src/main/java/com/loopers/interfaces/consumer/OrderEventConsumer.java
@@ -0,0 +1,23 @@
+package com.loopers.interfaces.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class OrderEventConsumer {
+
+    @KafkaListener(
+            topics = "order-events",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void consume(
+            @Header(KafkaHeaders.RECEIVED_KEY) String key,
+            String payload
+    ) {
+        log.info("Received order event: {}", payload);
+    }
+}
