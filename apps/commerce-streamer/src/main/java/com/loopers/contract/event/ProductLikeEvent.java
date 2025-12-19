d5c3c436ddeaa0c1c8b2a62f7f4669232d44df14
junoade <ajchoi0928@gmail.com>
2025-12-19 14:57:49 +0900

    dto: 타 멀티모듈 내 dto 추가

---
+++ apps/commerce-streamer/src/main/java/com/loopers/contract/event/ProductLikeEvent.java
@@ -0,0 +1,27 @@
+package com.loopers.contract.event;
+
+import org.slf4j.MDC;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record ProductLikeEvent (
+        String eventId,
+        Long userPkId,
+        Long productId,
+        LikeEventType eventType,
+        Instant occurredAt,
+        String traceId
+) {
+    public static ProductLikeEvent from(Long userPkId, Long productId, LikeEventType eventType) {
+        return new ProductLikeEvent(
+                UUID.randomUUID().toString(),
+                userPkId,
+                productId,
+                eventType,
+                Instant.now(),
+                MDC.get("traceId")
+        );
+    }
+}
+
